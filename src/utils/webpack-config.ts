c6e4c5af519ef11a9eea0ce344b2a50e56105179
ascoders <576625322@qq.com>
2018-03-26 16:23:56 +0800

    fix babel-preset-env to @babel/preset-env

--- src/utils/webpack-config.ts
+++ src/utils/webpack-config.ts
@@ -68,7 +68,7 @@ const config: webpack.Configuration = {
               babelrc: false,
               presets: [
                 [
-                  "env",
+                  "@babel/preset-env",
                   {
                     modules: false
                   }
