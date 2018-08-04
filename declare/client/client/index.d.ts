8fe73ee5f7e5d21f19d0e98470595bcd9b9199ef
ascoders <576625322@qq.com>
2018-08-04 15:33:19 +0800

    update

--- declare/client/client/index.d.ts
+++ declare/client/client/index.d.ts
@@ -1,4 +1,4 @@
 export declare const history: import("history").History;
 export declare const isDevelopment: boolean;
-export declare const projectConfig: import("../../../../../../../Users/huangziyi/workspace/pri/pri/src/utils/project-config-interface").ProjectConfig;
+export declare const projectConfig: import("../utils/project-config-interface").ProjectConfig;
 export declare type ProjectConfig = typeof projectConfig;
