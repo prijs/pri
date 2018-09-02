6f439c9f15d3b4bf998bc9c6d22110ca9e0bcd5e
ascoders <576625322@qq.com>
2018-09-02 20:54:00 +0800

    remove command plugin folder

--- src/pri-plugin-commanders/build.ts
+++
@@ -1,6 +0,0 @@
-import { globalState } from '../utils/global-state';
-import { tsPlusBabel } from '../utils/ts-plus-babel';
-
-export default async () => {
-  await tsPlusBabel(globalState.projectConfig.distDir);
-};
