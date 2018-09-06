541b44757e8b334a413662040e6f43c6039c7572
ascoders <576625322@qq.com>
2018-09-06 10:24:45 +0800

    lint error when test

--- src/built-in-plugins/command-test/index.ts
+++ src/built-in-plugins/command-test/index.ts
@@ -4,26 +4,18 @@ import { pri } from '../../node';
 import { log } from '../../utils/log';
 import { findNearestNodemodulesFile } from '../../utils/npm-finder';
 import { tempPath, testsPath } from '../../utils/structor-config';
-import { tsPlusBabel } from '../../utils/ts-plus-babel';
 
 export const CommandTest = async (instance: typeof pri) => {
-  const testJsTempDir = path.join(tempPath.dir, 'test-js-temp-dir');
-
-  log(`Build typescript files`);
-  execSync(`${findNearestNodemodulesFile('/.bin/rimraf')} ${testJsTempDir}`, { stdio: 'inherit' });
-
-  await tsPlusBabel(testJsTempDir);
-
   execSync(
     [
       findNearestNodemodulesFile('/.bin/nyc'),
       `--reporter lcov`,
       `--reporter text`,
       `--reporter json`,
-      `--exclude ${testJsTempDir}/${testsPath.dir}/**/*.js`,
+      `--exclude ${testsPath.dir}/**/*.ts`,
       `${findNearestNodemodulesFile('/.bin/ava')}`,
-      `--files ${path.join(instance.projectRootPath, `${testJsTempDir}/${testsPath.dir}/**/*.js`)}`,
-      `--failFast`
+      `--files ${path.join(instance.projectRootPath, `${testsPath.dir}/**/*.ts`)}`,
+      `--fail-fast`
     ].join(' '),
     {
       stdio: 'inherit',
@@ -33,9 +25,6 @@ export const CommandTest = async (instance: typeof pri) => {
 
   // remove .nyc_output
   execSync(`${findNearestNodemodulesFile('.bin/rimraf')} ${path.join(instance.projectRootPath, '.nyc_output')}`);
-
-  // remove test output temp dir
-  execSync(`${findNearestNodemodulesFile('/.bin/rimraf')} ${testJsTempDir}`, { stdio: 'inherit' });
 
   // Open test html in brower
   log(
@@ -58,7 +47,7 @@ export default async (instance: typeof pri) => {
     name: 'test',
     description: 'Run tests.',
     action: async () => {
-      await instance.project.lint(false);
+      await instance.project.lint();
       await instance.project.ensureProjectFiles();
       await instance.project.checkProjectFiles();
       await CommandTest(instance);
