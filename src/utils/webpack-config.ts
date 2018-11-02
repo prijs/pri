a0ea7fdb9d8f44ef4654567385cc6b168564d9fd
ascoders <576625322@qq.com>
2018-11-02 15:47:35 +0800

    fix bug

--- src/utils/webpack-config.ts
+++ src/utils/webpack-config.ts
@@ -94,7 +94,7 @@ export const getWebpackConfig = async (opts: IOptions) => {
   const config: webpack.Configuration = {
     mode: opts.mode,
     entry: opts.entryPath,
-    devtool: opts.mode === 'development' ? 'source-map' : null,
+    devtool: opts.mode === 'development' ? 'source-map' : false,
     output: {
       path: distDir,
       filename: outFileName,
