f38e8c49408d51e0aae8473a328f1dccf42bc7ad
ascoders <576625322@qq.com>
2018-11-18 20:02:04 +0800

    back to webpack-dev-server

--- declare/client/client/index.d.ts
+++ declare/client/client/index.d.ts
@@ -1,4 +1,4 @@
-export declare const history: import("history").History;
+export declare const history: import("history").History<any>;
 export declare const isDevelopment: boolean;
 export declare const projectConfig: import("../utils/project-config-interface").ProjectConfig;
 export declare type ProjectConfig = typeof projectConfig;
