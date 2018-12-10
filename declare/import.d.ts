da224239af511cf85ac6870dda40b577e2f61cfd
ascoders <576625322@qq.com>
2018-12-10 21:35:47 +0800

    support worker-loader

--- declare/import.d.ts
+++ declare/import.d.ts
@@ -1,2 +1,10 @@
 declare module '*.json';
 declare module '*.html';
+
+declare module 'worker-loader!*' {
+  class WebpackWorker extends Worker {
+    constructor();
+  }
+
+  export default WebpackWorker;
+}
