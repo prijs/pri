897f802c2b799747eb0007cbc4cd14275116d41a
ascoders <576625322@qq.com>
2019-06-04 14:49:02 +0800

    do not create docs/basic if docs already exist

--- src/built-in-plugins/ensure-project-files/plugin/ensure-component.ts
+++ src/built-in-plugins/ensure-project-files/plugin/ensure-component.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as fs from 'fs-extra';
 import { componentEntry, docsPath, pri, srcPath } from '../../../node';
 import { prettierConfig } from '../../../utils/prettier-config';
 import { ensureTest } from './ensure-project';
@@ -30,28 +31,34 @@ function ensureEntryFile() {
 }
 
 function ensureDocs() {
-  const basicDocsPath = path.join(pri.sourceRoot, docsPath.dir, 'basic.tsx');
-  const relativeToEntryPath = path.relative(
-    path.parse(basicDocsPath).dir,
-    path.join(pri.sourceRoot, srcPath.dir, 'index')
-  );
-  pri.project.addProjectFiles({
-    fileName: basicDocsPath,
-    pipeContent: async text => {
-      if (text) {
-        return text;
+  const docsAbsolutePath = path.join(pri.sourceRoot, docsPath.dir);
+
+  if (!fs.existsSync(docsAbsolutePath)) {
+    const basicDocsPath = path.join(pri.sourceRoot, docsPath.dir, 'basic.tsx');
+
+    const relativeToEntryPath = path.relative(
+      path.parse(basicDocsPath).dir,
+      path.join(pri.sourceRoot, srcPath.dir, 'index')
+    );
+
+    pri.project.addProjectFiles({
+      fileName: basicDocsPath,
+      pipeContent: async text => {
+        if (text) {
+          return text;
+        }
+
+        const prettier = await import('prettier');
+        return prettier.format(
+          `
+            import * as React from "react"
+            import Component from "${relativeToEntryPath}"
+  
+            export default () => <Component />
+          `,
+          { ...prettierConfig, parser: 'typescript' }
+        );
       }
-
-      const prettier = await import('prettier');
-      return prettier.format(
-        `
-          import * as React from "react"
-          import Component from "${relativeToEntryPath}"
-
-          export default () => <Component />
-        `,
-        { ...prettierConfig, parser: 'typescript' }
-      );
-    }
-  });
+    });
+  }
 }
