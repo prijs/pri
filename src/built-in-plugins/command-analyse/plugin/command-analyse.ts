d0ff2abb8c056f39a0755e04a62133881e88427f
ascoders <576625322@qq.com>
2019-06-13 15:11:03 +0800

    support analyse for component

--- src/built-in-plugins/command-analyse/plugin/command-analyse.ts
+++ src/built-in-plugins/command-analyse/plugin/command-analyse.ts
@@ -1,20 +1,28 @@
 import * as webpackBundleAnalyzer from 'webpack-bundle-analyzer';
+import * as path from 'path';
 import { analyseProject } from '../../../utils/analyse-project';
 import { createEntry } from '../../../utils/create-entry';
 import { spinner } from '../../../utils/log';
 import { runWebpack } from '../../../utils/webpack';
+import { globalState } from '../../../utils/global-state';
+import { componentEntry } from '../../../utils/structor-config';
 
 export const commandAnalyse = async () => {
-  const result = await spinner('Analyse project', async () => {
-    const analyseInfo = await analyseProject();
-    const entryPath = await createEntry();
-    return { analyseInfo, entryPath };
-  });
+  let entryPath: string = null;
+
+  if (globalState.sourceConfig.type === 'project') {
+    const result = await spinner('Analyse project', async () => {
+      return { analyseInfo: await analyseProject(), entryPath: await createEntry() };
+    });
+    ({ entryPath } = result);
+  } else if (globalState.sourceConfig.type === 'component') {
+    entryPath = path.join(globalState.sourceRoot, path.format(componentEntry));
+  }
 
   // Build project
   await runWebpack({
     mode: 'production',
-    entryPath: result.entryPath,
+    entryPath,
     pipeConfig: async config => {
       config.plugins.push(new webpackBundleAnalyzer.BundleAnalyzerPlugin());
       return config;
