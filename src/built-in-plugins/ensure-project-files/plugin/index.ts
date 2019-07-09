8f5e3a2ef853b8d0acf5c131c1e0bdaefc30e26d
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2019-07-09 16:08:39 +0800

    update

--- src/built-in-plugins/ensure-project-files/plugin/index.ts
+++ src/built-in-plugins/ensure-project-files/plugin/index.ts
@@ -60,7 +60,6 @@ function ensureTsconfig() {
             skipLibCheck: true,
             outDir: globalState.sourceConfig.distDir,
             baseUrl: '.',
-            incremental: true,
             lib: ['dom', 'es5', 'es6', 'scripthost', 'es2018.promise'],
             paths: {
               [`${PRI_PACKAGE_NAME}/*`]: [PRI_PACKAGE_NAME, path.join(tempTypesPath.dir, '*')],
