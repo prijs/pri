eb93a641aefa627b30d4616eca539913deae753c
linfeng <bensheng.xbs@alibaba-inc.com>
2019-10-09 14:00:05 +0800

    fix: remove prettierrc file from cleanUncessaryFields

--- src/node/clean-unecessary-files.ts
+++ src/node/clean-unecessary-files.ts
@@ -3,6 +3,5 @@ import * as path from 'path';
 import { globalState } from '../utils/global-state';
 
 export async function cleanUncessaryFields() {
-  await fs.remove(path.join(globalState.projectRootPath, '.prettierrc'));
   await fs.remove(path.join(globalState.projectRootPath, 'tsconfig.jest.json'));
 }
