c035022d8967cef0cf72f2dddfe7fc5402adb27c
ascoders <576625322@qq.com>
2019-11-13 17:50:03 +0800

    feat: deploy 16

--- src/utils/type-checker.ts
+++ src/utils/type-checker.ts
@@ -4,36 +4,36 @@ import { pri } from '../node';
 import { logFatal, logInfo, logSuccess } from './log';
 
 export function typeChecker() {
-    const stashFileCnt = +execSync('git diff --cached --numstat --diff-filter=ACM | wc -l').toString('utf8');
+  const stashFileCnt = +execSync('git diff --cached --numstat --diff-filter=ACM | wc -l').toString('utf8');
 
-    if (stashFileCnt > 0) {
-        let debugFiles = '';
+  if (stashFileCnt > 0) {
+    let debugFiles = '';
 
-        try {
-            debugFiles = execSync(
-                "grep -in '@DEBUG' --include *.ts --include *.tsx --include *.js --include *.scss --include *.css $(git diff --cached --name-only --diff-filter=ACM) /dev/null",
-                {
-                    shell: 'bash',
-                    stdio: [0, 1],
-                },
-            ).toString('utf8');
-            // eslint-disable-next-line no-empty
-        } catch (e) {}
-
-        if (debugFiles) {
-            logFatal('@DEBUG identifier exists, the commit is terminated');
-            process.exit(1);
-        }
-    }
-
-    logInfo('Checking TypeScript type, please wait');
     try {
-        execSync(`${pri.projectRootPath}/node_modules/.bin/tsc -p . || exit 1`, {
-            stdio: [0, 1, 2],
-        });
-    } catch (e) {
-        logFatal('Type checks error!');
-        process.exit(1);
+      debugFiles = execSync(
+        "grep -in '@DEBUG' --include *.ts --include *.tsx --include *.js --include *.scss --include *.css $(git diff --cached --name-only --diff-filter=ACM) /dev/null",
+        {
+          shell: 'bash',
+          stdio: [0, 1],
+        },
+      ).toString('utf8');
+      // eslint-disable-next-line no-empty
+    } catch (e) {}
+
+    if (debugFiles) {
+      logFatal('@DEBUG identifier exists, the commit is terminated');
+      process.exit(1);
     }
-    logSuccess('Type checks passed!');
+  }
+
+  logInfo('Checking TypeScript type, please wait');
+  try {
+    execSync(`${pri.projectRootPath}/node_modules/.bin/tsc -p . || exit 1`, {
+      stdio: [0, 1, 2],
+    });
+  } catch (e) {
+    logFatal('Type checks error!');
+    process.exit(1);
+  }
+  logSuccess('Type checks passed!');
 }
