a6729e1ae35a0eba622ba81d594938fb3bb349a0
linfeng <bensheng.xbs@alibaba-inc.com>
2019-12-30 10:52:57 +0800

    chore: update version to 4.0.2

--- src/built-in-plugins/project-analyse-pages/plugin/index.ts
+++ src/built-in-plugins/project-analyse-pages/plugin/index.ts
@@ -140,33 +140,31 @@ pri.project.onCreateEntry((analyseInfo: IResult, entry) => {
 
   entry.pipeAppComponent(async entryComponent => {
     return `
-        ${(
-          await Promise.all(
-            analyseInfo.projectAnalysePages.pages
-              .filter(page => !!page.file)
-              .map(async page => {
-                const pageRequirePath = normalizePath(
-                  path.relative(path.join(pri.projectRootPath, tempPath.dir), path.join(page.file.dir, page.file.name)),
-                );
+        ${(await Promise.all(
+          analyseInfo.projectAnalysePages.pages
+            .filter(page => !!page.file)
+            .map(async page => {
+              const pageRequirePath = normalizePath(
+                path.relative(path.join(pri.projectRootPath, tempPath.dir), path.join(page.file.dir, page.file.name)),
+              );
 
-                const importCode = `import(/* webpackChunkName: "${
-                  page.chunkName
-                }" */ "${pageRequirePath}").then(code => {
+              const importCode = `import(/* webpackChunkName: "${
+                page.chunkName
+              }" */ "${pageRequirePath}").then(code => {
                 const filePath = "${path.format(page.file)}"
 
                 ${await entry.pipe.get('afterPageLoad', '')}
                 ${await entry.pipe.get('returnPageInstance', 'return code.default')}
               })`;
 
-                return `
+              return `
               const ${page.componentName} = Loadable<any, any>({
                 loader: () => ${importCode},
                 loading: (): any => null
               })\n
             `;
-              }),
-          )
-        ).join('\n')}
+            }),
+        )).join('\n')}
           ${entryComponent}
       `;
   });
@@ -186,24 +184,22 @@ pri.project.onCreateEntry((analyseInfo: IResult, entry) => {
 
   entry.pipeAppRoutes(async renderRoutes => {
     return `
-        ${(
-          await Promise.all(
-            analyseInfo.projectAnalysePages.pages.map(async page => {
-              if (page.file) {
-                return `
+        ${(await Promise.all(
+          analyseInfo.projectAnalysePages.pages.map(async page => {
+            if (page.file) {
+              return `
               <${await entry.pipe.get('commonRoute', 'Route')} exact path="${page.routerPath}" component={${
-                  page.componentName
-                }} />\n
+                page.componentName
+              }} />\n
             `;
-              }
-              if (page.redirect) {
-                return `
+            }
+            if (page.redirect) {
+              return `
               <Redirect from="${page.routerPath}" to="${page.redirect}" />\n
             `;
-              }
-            }),
-          )
-        ).join('\n')}
+            }
+          }),
+        )).join('\n')}
         ${renderRoutes}
       `;
   });
