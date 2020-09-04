b1e7c9b502a53f44ae398f33c3763b51d0783a52
月满 <hjm248891@alibaba-inc.com>
2020-09-04 10:26:43 +0800

    fix: lint on def

--- src/built-in-plugins/command-lint/plugin/index.ts
+++ src/built-in-plugins/command-lint/plugin/index.ts
@@ -1,11 +1,25 @@
 import { pri } from '../../../node';
 import text from '../../../utils/text';
+import { logFatal } from '../../../utils/log';
+
+export interface LintOption {
+  def?: boolean;
+}
 
 pri.commands.registerCommand({
   name: ['lint'],
   description: text.commander.lint.description,
-  action: async () => {
-    const { CommandLint } = await import('./run-lint');
-    await CommandLint();
+  options: {
+    def: {
+      description: 'lint on def, no git context',
+    },
+  },
+  action: async (option: LintOption) => {
+    try {
+      const { CommandLint } = await import('./run-lint');
+      await CommandLint(option);
+    } catch (error) {
+      logFatal(error);
+    }
   },
 });
